baddf97a5a0abbfd57c172b01e4fbdaaa65c93db
dmoya0215 <dmoya0215@gmail.com>
2026-08-30 19:29:47 -0500

    Implementa auditoria y exportacion de informes

--- apps/auditoria/urls.py
+++ apps/auditoria/urls.py
@@ -2,5 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.auditoria, name='auditoria'),
+    path("", views.auditoria, name="auditoria"),
+
+    path(
+        "exportar/excel/",
+        views.exportar_excel,
+        name="exportar_excel"
+    ),
+
+    path(
+        "exportar/pdf/",
+        views.exportar_pdf,
+        name="exportar_pdf"
+    ),
 ]
