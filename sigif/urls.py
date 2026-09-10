96521a123a10be31af436ad30451eb745de19091
jmorenoyepes-ai <jmorenoyepes@gmail.com>
2026-09-10 14:50:08 -0500

    errores

--- sigif/urls.py
+++ sigif/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 from rest_framework.authtoken.views import obtain_auth_token
 from apps.api.views import LogoutView
 from core.error_views import error_400, error_403, error_404, error_500
-# pyrefly: ignore [missing-import]
+
+
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -41,18 +41,7 @@ urlpatterns = [
     path('api/auth/logout/', LogoutView.as_view(), name='api_logout')
 ]
 
-# Manejadores de errores HTTP personalizados
 handler400 = error_400
 handler403 = error_403
 handler404 = error_404
 handler500 = error_500
-
-# URLs de previsualización (solo en modo DEBUG)
-if settings.DEBUG:
-    urlpatterns += [
-        path('errores/preview/400/', lambda req: error_400(req), name='preview_400'),
-        path('errores/preview/403/', lambda req: error_403(req), name='preview_403'),
-        path('errores/preview/404/', lambda req: error_404(req), name='preview_404'),
-        path('errores/preview/500/', lambda req: error_500(req), name='preview_500'),
-    ]
-
